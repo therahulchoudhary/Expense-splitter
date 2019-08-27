e2ff849f22588b2593e278ee748b97c6bc1a09e9
Your Name <you@example.com>
2019-08-27 18:01:49 +0530

    This commit contains the dynamic fields.

--- expense-splitter/src/app/app.module.ts
+++ expense-splitter/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,9 @@ import { SplitFormComponent } from './split-form/split-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- expense-splitter/src/app/split-form/split-form.component.ts
+++ expense-splitter/src/app/split-form/split-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-split-form',
@@ -6,9 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./split-form.component.css']
 })
 export class SplitFormComponent implements OnInit {
+  personsForm : FormGroup;
+  individualTotal : number=0;
+  tcode : string;
+  arrayValue : string;
+  previousValue : number=0;
+  constructor(private fb : FormBuilder) {
+    var i;
+    // this.personsForm = this.fb.group({
+    //   howMuch: this.fb.array([
+    //     this.fb.control(null)
+    //   ])      
+    // });   
+    this.personsForm = this.fb.group({
+      person: this.fb.array([
+        this.fb.control(null)
+      ]),
+      howMuch: this.fb.array([
+        this.fb.control(null)
+      ])
+    });
+  }
+  // findIndividualTotal(){
+  //   var temp = parseInt(this.tcode);
+  //   this.individualTotal = temp;
+  //   this.previousValue = this.individualTotal;
+  //   console.log(this.individualTotal);
+  // }
+  // countIndividualTotal(){
+  //   var temp = parseInt(this.tcode);
+  //   if(this.individualTotal!=this.previousValue){
+  //     this.individualTotal +=temp;
+  //   }
+  //   else{
+  //     this.individualTotal =this.previousValue;
+  //   }
+  // }
+  addMoreExpense(){
+    (this.personsForm.get('howMuch') as FormArray).push(
+      this.fb.control(null)
+    );
+  }
+  addMorePerson(){
+    var i;
+    var limit = parseInt(this.tcode);
+    for(i=0;i<limit;i++){
+      (this.personsForm.get('person') as FormArray).push(
+        this.fb.control(null)
+      )
+    }
+  }
+  removeExpense() {
+    (this.personsForm.get('howMuch') as FormArray).removeAt(1);
+  }
 
-  constructor() { }
-
+  getExpenseFormControls(): AbstractControl[] {
+    return (<FormArray> this.personsForm.get('howMuch')).controls
+  }
+  getPersonFormControls(): AbstractControl[] {
+    return (<FormArray> this.personsForm.get('person')).controls
+  }
   ngOnInit() {
   }
 
